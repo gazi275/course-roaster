fbb2d5c5b92be9676a2dea2724c3ffd9a2c0e9d6
gazi275 <gaziazad270@gmail.com>
2023-09-16 09:21:44 +0600

    all calculation and toast are added

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,19 +1,29 @@
 
 
-const Cart = (select) => {
+const Cart = ({ select, total, remaine, price }) => {
+
     return (
         <div className='  bg-white rounded-3xl mx-auto'>
-            <div className='border-2 w-[380px] h-[410px] text-center ' >
-                <h1 className="text-2xl font-bold text-blue-600 mt-4" >Credit Hour Remaining  :</h1>
-               <hr className="border-b-2 border-slate-100 w-full " />
+            <div className='border-2 w-[380px] h-[350px] px-8 ' >
+                <h1 className="text-2xl font-bold text-blue-600 my-6" >Credit Hour Remaining  :{remaine}</h1>
+                <hr className="border-b-2 border-slate-200 w-full " />
 
-               <h1>Course Name</h1>
-               <ol>
-               
-               
-               </ol>
+                <h1 className="text-xl font-semibold">Course Name </h1>
+                <ol className="list-decimal list-inside">
+                    {
+                        select.map(card =>
+
+                            <li className="items-start" key={card.id}>{card.course_title}</li>
+                        )
+                    }
+                     </ol>
+                <hr className="border-b-2 border-slate-100 w-full mt-6 " />
+                <h3 className="text-lg from-neutral-600 my-4">Total Credit Hour :{total}</h3>
+                <hr className="border-b-2 border-slate-100 w-full mt-6 " />
+                <h3 className="text-lg font-medium mt-4">Total Price :{price}</h3>
             </div>
         </div>
+
     );
 };
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,42 +1,78 @@
-import { data } from "autoprefixer";
+
 import { useEffect } from "react";
 import { useState } from "react";
 import Card from "./card";
 import Cart from "./cart";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 
 const Home = () => {
-    const [courses,setCourses]=useState([]);
-    const [select,setSelect]=useState([])
-    useEffect(()=>{
-        fetch('blog.json')
-        .then (res=>res.json())
-        .then (data=> setCourses(data));},
-        [])
+  const [courses, setCourses] = useState([]);
+  const [select, setSelect] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [remaine, setRemaine] = useState(0);
+  const [price, setPrice] = useState(0);
+  useEffect(() => {
+    fetch('blog.json')
+      .then(res => res.json())
+      .then(data => setCourses(data));
+  },
+    [])
 
-      const handlecart=(card) =>{
-        setSelect([...select,card])
-        
+  const handlecart = (card) => {
+    const exist = select.find(item => item.id == card.id)
+    let count = card.credit;
+    let totalPrice = card.price
 
+    if (exist) {
+      return toast("Already Token!");
+    }
+    else {
+      select.forEach(item => {
+        count = count + item.credit;
+        totalPrice = totalPrice + item.price;
+      })
+
+      const remaining = 20 - count;
+      if (remaining < 0) {
+        return toast("you have no longer Credit hour!");
       }
-console.log(select);
-        
-       
-    return (
-        <div className="flex  gap-16 justify-center" >
-           <div className="grid grid-cols-3 ">
-             {
-                courses.map(card=><Card key={card.id} card={card} handlecart={handlecart}></Card>)
-                        
-            }
-            </div>
-            <div className="my-10">
-               <Cart select={select}></Cart>
-                </div> 
-           
-        </div>
-    );
+      else {
+        setPrice(totalPrice);
+        setRemaine(remaining);
+        setTotal(count);
+        setSelect([...select, card])
+      }
+
+    }
+
+
+
+  }
+
+
+
+  return (
+   <>
+    <div className="flex  gap-16 justify-center" >
+      <div className="grid grid-cols-3 ">
+
+        {
+          courses.map(card => <Card key={card.id} card={card} handlecart={handlecart}></Card>)
+
+        }
+      </div>
+      <div className="my-10">
+        <Cart select={select} total={total} remaine={remaine} price={price}></Cart>
+      </div>
+     
+    </div>
+
+    <ToastContainer/>
+   </>
+  );
 };
 
 export default Home;
